8efa32f8fc56f3529952f193321b19162d126653
Justin Yu <justinxyu1128@gmail.com>
2021-04-05 00:35:26 -0700

    added instructions for creating dark mode button

---
+++ app.js
@@ -0,0 +1,12 @@
+/* dark button 
+-give an id to the dark mode button
+-create a variable 'darkmodebtn' using document.querySelector on the dark mode button's id
+-create a function darkMode that changes the styling of the background to fit a dark mode
+-create a function darkBtn that uses e event object to change the styling of the button
+-search google for a way to alternate what happens when the button is clicked, maybe something to do with changing id?
+-use darkmodebtn.addEventListener('click', darkMode);
+-use darkmodebtn.addEventListener('click', darkBtn);
+
+*/
+/* note taking button */
+/* note menu */
